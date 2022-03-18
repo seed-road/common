26c590d35a029994b7715493633421e937a7c9a5
BAILLEUL <bailleul.sacha@gmail.com>
2022-03-18 14:53:34 +0100

    [feat]: Change write aggregate dto events

--- SeedRoad.Common.Core.Domain/Definitions/WriteAggregateDto.cs
+++ SeedRoad.Common.Core.Domain/Definitions/WriteAggregateDto.cs
@@ -4,17 +4,18 @@ namespace SeedRoad.Common.Core.Domain.Definitions;
 
 public abstract class WriteAggregateDto : IAggregateDto
 {
-    public WriteAggregateDto(IAggregate aggregate)
+
+    public WriteAggregateDto(IEnumerable<IDomainEvent> events)
     {
-        _events = aggregate.Events.ToList();
+        _events = events.ToList();
     }
+
 
     public IReadOnlyList<IDomainEvent>? Events => _events.AsReadOnly();
     protected List<IDomainEvent>? _events;
 
     protected WriteAggregateDto()
     {
-        
     }
 
     public void ClearEvents()
