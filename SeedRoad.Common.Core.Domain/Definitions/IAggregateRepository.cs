3d86fbf385cda84c220ac61a4635dd09304c9f03
Landulu <mantoine1@myges.fr>
2022-04-09 17:52:49 +0200

    [fix] get by id async

--- SeedRoad.Common.Core.Domain/Definitions/IAggregateRepository.cs
+++ SeedRoad.Common.Core.Domain/Definitions/IAggregateRepository.cs
@@ -2,7 +2,7 @@ namespace SeedRoad.Common.Core.Domain.Definitions;
 
 public interface IAggregateRepository<TId, in TWriteAggregateDto, TReadAggregateDto> where TWriteAggregateDto : IAggregateDto
 {
-    public Task<TId> SetAsync(TWriteAggregateDto aggregate);
+    public Task<TId> SetAsync(TWriteAggregateDto dto);
     public Task<TReadAggregateDto?> FindByIdAsync(TId id);
     public Task RemoveAsync(TId id);
     public Task<TId> NextIdAsync();
