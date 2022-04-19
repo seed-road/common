9889324a18d6202aff6ab9670fbb7b97fc22c807
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-04-19 12:37:13 +0200

    [feat]: Change remove async method

--- SeedRoad.Common.Core.Domain/Definitions/IAggregateRepository.cs
+++ SeedRoad.Common.Core.Domain/Definitions/IAggregateRepository.cs
@@ -1,9 +1,10 @@
 namespace SeedRoad.Common.Core.Domain.Definitions;
 
-public interface IAggregateRepository<TId, in TWriteAggregateDto, TReadAggregateDto> where TWriteAggregateDto : IAggregateDto
+public interface IAggregateRepository<TId, in TWriteAggregateDto, TReadAggregateDto>
+    where TWriteAggregateDto : IAggregateDto
 {
     public Task<TId> SetAsync(TWriteAggregateDto dto);
     public Task<TReadAggregateDto?> FindByIdAsync(TId id);
-    public Task RemoveAsync(TId id);
+    public Task RemoveAsync(TWriteAggregateDto dto);
     public Task<TId> NextIdAsync();
 }
