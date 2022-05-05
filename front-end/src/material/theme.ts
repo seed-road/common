05399c2c1b20e967871cfc593879170b2525f8f4
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-05-06 01:11:20 +0200

    [feat]: Add common theming

---
+++ front-end/src/material/theme.ts
@@ -0,0 +1,18 @@
+﻿import {createTheme, Theme} from "@mui/material";
+import {amber, brown, green} from "@mui/material/colors";
+
+export const getThemeBy = (prefersDarkMode: boolean): Theme => {
+    return createTheme({
+        palette: prefersDarkMode ?
+            {
+                mode: 'dark',
+                primary: amber,
+                secondary: green,
+            } :
+            {
+                mode: 'light',
+                primary: brown,
+                secondary: green,
+            }
+    });
+}
