d2d480b352d53e7f159d358e6162cdbfbc838540
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-05-18 12:04:20 +0200

    [feat] : Update forms

---
+++ front-end/src/forms/Field.tsx
@@ -0,0 +1,26 @@
+﻿import {FormikContextType} from "formik/dist/types";
+import {ReactElement} from "react";
+import {TextField, TextFieldProps} from "@mui/material";
+import _ from "lodash";
+
+export interface FieldProps<TValue> {
+    formKey: keyof TValue,
+    label?: string,
+    formik: FormikContextType<TValue>
+}
+
+export function Field<TValue>({formik, formKey, label, ...props}: FieldProps<TValue> & TextFieldProps): ReactElement {
+    return (
+        <TextField
+            id={`${formKey}`}
+            name={`${formKey}`}
+            label={label ?? _.capitalize(`${formKey}`)}
+            fullWidth
+            value={formik.values[formKey]}
+            onChange={formik.handleChange}
+            error={formik.touched[formKey] && Boolean(formik.errors[formKey])}
+            helperText={formik.touched[formKey] && formik.errors[formKey]}
+            {...props}
+        />
+    );
+}
