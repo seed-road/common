ac063dd894e4e289707e328466d4e204d1afdc9f
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-06-10 12:43:37 +0200

    [feat] : Add create update delete exchange

--- SeedRoad.Common.Messaging/Configurations/CreateUpdateDeleteExchange.cs
+++ SeedRoad.Common.Messaging/Configurations/CreateUpdateDeleteExchange.cs
@@ -1,8 +1,9 @@
 ﻿namespace SeedRoad.Common.Messaging.Configurations;
 
-public class CreateUpdateExchange : IExchange
+public class CreateUpdateDeleteExchange : IExchange
 {
     public string Name { get; set; } = null!;
     public string CreatedRoutingKey { get; set; } = null!;
     public string UpdatedRoutingKey { get; set; } = null!;
+    public string DeletedRoutingKey { get; set; } = null!;
 }
