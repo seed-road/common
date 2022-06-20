53eb551781cc9f8c1f296e497f485f032656bf37
sbailleul <44065690+sbailleul@users.noreply.github.com>
2022-06-20 23:54:36 +0200

    [feat]: Add create exchange

---
+++ SeedRoad.Common.Messaging/Configurations/CreateExchange.cs
@@ -0,0 +1,7 @@
+﻿namespace SeedRoad.Common.Messaging.Configurations;
+
+public class CreateExchange : IExchange, ICreateExchange
+{
+    public string Name { get; set; } = null!;
+    public string CreatedRoutingKey { get; set; } = null!;
+}
